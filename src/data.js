4af205844b8d9193eb372aa99ef8a9b828a02255
FsmDeveloper <smshakil1600@gmail.com>
2018-05-14 03:53:11 +0600

    template ready

--- src/data.js
+++ src/data.js
@@ -3,10 +3,31 @@ const firestore = firebase.firestore();
 const settings = {/* your settings... */ timestampsInSnapshots: true };
 firestore.settings(settings);
 
-var db = firebase.firestore();
+const db = firebase.firestore();
 
-db.collection("users").get().then((querySnapshot) => {
+db.collection("posts").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data().born}`);
+        console.log(doc);
+        
+        console.log(`${doc.id} => ${doc.data().author}`);
     });
 });
+
+window.onload = () => {
+    let root = document.querySelector("#root");
+
+    let container = document.createElement("div");
+    container.innerHTML = 
+    `
+    <div class="jumbotron">
+        <h1>Bootstrap Tutorial</h1> 
+        <p>Bootstrap is the most popular HTML, CSS...</p> 
+    </div>
+
+
+    `;
+
+    root.appendChild(container);
+
+    
+}
